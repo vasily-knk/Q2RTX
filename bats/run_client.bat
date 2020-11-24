f9aae3bd9e2690d369294b3e720f4a5a99faf746
vasily-knk <vasily.knk@gmail.com>
2020-11-25 00:38:33 +0300

    restored fake webstream client

--- bats/run_client.bat
+++ bats/run_client.bat
@@ -1,4 +1,4 @@
-PATH=%PATH%;C:\work\simlabs-repo\bin\debug;C:\work\webstream\ws_server\build_x64\Debug
+PATH=%PATH%;%SIMLABS_REPO_VR%\bin\fastdebug
 cd ..
 
 start q2rtx.exe +set game client_game +map base1
